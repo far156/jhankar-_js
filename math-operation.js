f6999aaf6f9545f04d0487a22d91f067342b356f
Farzana Akter <67409808+far156@users.noreply.github.com>
2022-05-20 21:43:40 +0600

    Add files via upload

---
+++ math-operation.js
@@ -0,0 +1,16 @@
+var price1=21;
+var price2=10;
+var sum=price1+price2;
+var difference=price1-price2;
+var division=price1/price2;
+var multiplication=price1*price2;
+console.log(sum);
+console.log(difference);
+console.log(division);
+console.log(multiplication);
+var newPrice=price1*3;
+ price1=price1+20;
+ //variable ak bar use korley porey var likhbona
+ price1 +=44;
+ 
+console.log(newPrice);
---
+++ mathematical-operation.js
@@ -0,0 +1,10 @@
+var peyiajPrice=43;
+var eggPrice=12;
+console.log(eggPrice);
+console.log(peyiajPrice);
+console.log(peyiajPrice+eggPrice);
+var totalPrice=peyiajPrice+eggPrice;
+console.log(totalPrice);
+var priceDifferance=peyiajPrice-eggPrice;
+console.log(priceDifferance);
+ 

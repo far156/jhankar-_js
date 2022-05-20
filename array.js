ad77ca896f45594bfb2a68ba237a34cab68c8c90
Farzana Akter <67409808+far156@users.noreply.github.com>
2022-05-21 00:55:16 +0600

    Add files via upload

---
+++ array.js
@@ -0,0 +1,11 @@
+var friendAge=[11,12,17];
+var muricahnachur=[12,12,13];
+var nayika=['mahi','subon'];
+var oddNumbers=[1,2,3,4];
+var vowels=['a','e','i','o','u'];
+console.log(vowels);
+console.log(oddNumbers.length);
+var vowelsCount=vowels.length;
+console.log(vowelsCount);
+
+//array upadan jonno length use korey
---
+++ mutiple-condition.js
@@ -0,0 +1,10 @@
+var gotJob=false;
+var moneySaved=15000;
+if(gotJob==true&& moneySaved>200000){
+    console.log('cholo biye kori!!');
+}
+else{
+    console.log('biye nai')
+}
+//&& holey duita shorto
+

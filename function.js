0893f1aa2ef289e88f51a0e0ee2d7199edbe0ddd
Farzana Akter <67409808+far156@users.noreply.github.com>
2022-05-24 01:17:14 +0600

    Add files via upload

---
+++ function.js
@@ -0,0 +1,16 @@
+//functiob decleare
+function startFan(){
+    console.log('walk towards');
+    console.log('press the switch');
+
+} 
+//function call the function
+//jokhon drkar tokhon daka
+
+startFan();
+
+console.log('i eat rice');
+console.log('i like sleep');
+console.log('take shower');
+startFan();
+startFan();
---
+++ parameter.js
@@ -0,0 +1,13 @@
+function bringSingara(taka){
+    console.log('ano',taka);
+    console.log('mam ano');
+    var singaraPrice=10;
+    var singaraQuality=taka/singaraPrice;
+    return singaraQuality;
+
+
+}
+var money=250;
+ var singara=bringSingara(money);
+
+
